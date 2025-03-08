74964ec444500f7625de18be369a417ad7b413c9
Hieu-BuiMinh <minhhieu2122000@gmail.com>
2025-03-08 23:53:54 +0700

    feat[main]: config next js 15, tailwind css 4, eslint plugin, husky check before commiting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,64 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// https://github.dev/sonsu95/next15-eslint9-template/
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+	baseDirectory: __dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+	{
+		ignores: ['node_modules/**', '.next/**', 'dist/**', 'postcss.config.mjs'],
+	},
+	{
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			parser: '@typescript-eslint/parser',
+			parserOptions: {
+				project: './tsconfig.json',
+			},
+			globals: {
+				browser: true,
+				es2022: true,
+				node: true,
+			},
+		},
+	},
+	...compat.extends('next/core-web-vitals', 'next/typescript'),
+	{
+		files: ['**/*.{js,jsx,ts,tsx}'],
+		plugins: {
+			prettier: eslintPluginPrettier,
+		},
+		rules: {
+			'prettier/prettier': 'warn',
+			'no-console': ['warn', { allow: ['warn', 'error'] }],
+			'@typescript-eslint/no-unused-vars': [
+				'warn',
+				{
+					argsIgnorePattern: '^_',
+					varsIgnorePattern: '^_',
+				},
+			],
+			'@typescript-eslint/no-explicit-any': 'warn',
+			'@typescript-eslint/consistent-type-imports': 'error',
+			'import/order': [
+				'warn',
+				{
+					groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+					'newlines-between': 'always',
+					alphabetize: { order: 'asc', caseInsensitive: true },
+				},
+			],
+		},
+	},
+]
 
-export default eslintConfig;
+export default eslintConfig
---
+++ prettier.config.mjs
@@ -0,0 +1,23 @@
+const config = {
+	arrowParens: 'always',
+	bracketSpacing: true,
+	endOfLine: 'lf',
+	htmlWhitespaceSensitivity: 'css',
+	insertPragma: false,
+	singleAttributePerLine: false,
+	bracketSameLine: false,
+	jsxBracketSameLine: false,
+	jsxSingleQuote: false,
+	printWidth: 120,
+	proseWrap: 'preserve',
+	quoteProps: 'as-needed',
+	requirePragma: false,
+	semi: false,
+	singleQuote: true,
+	tabWidth: 4,
+	trailingComma: 'es5',
+	useTabs: true,
+	plugins: ['prettier-plugin-tailwindcss'],
+}
+
+export default config
